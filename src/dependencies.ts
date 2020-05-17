ef152401d44dd481f07956374d51d163c8b986b8
Tomer Aberbach <tomeraberbach@gmail.com>
2020-05-16 22:26:52 -0400

    feat: multiple package paths and automatic find

--- src/dependencies.ts
+++ src/dependencies.ts
@@ -1,6 +1,6 @@
 import { dirname, relative, isAbsolute } from 'path'
-import { readFileSync } from 'fs'
-import { sync as findUp } from 'find-up'
+import { promises as fs } from 'fs'
+import findUp from 'find-up'
 
 /**
  * Determines if the `child` path is under the `parent` path.
@@ -15,9 +15,9 @@ function isInDirectory(parent: string, child: string): boolean {
  * current working directory. If the current working directory is in a git repository, then package.json
  * files outside of the git repository will not be yielded.
  */
-export function* findPackagePaths(): Generator<string> {
+export async function* findPackagePaths(): AsyncGenerator<string> {
     // Find git root if in git repository
-    const gitDirectoryPath: string | undefined = findUp('.git', { type: 'directory' })
+    const gitDirectoryPath: string | undefined = await findUp('.git', { type: 'directory' })
     const gitRootPath: string | undefined = gitDirectoryPath === undefined
         ? undefined
         : dirname(gitDirectoryPath)
@@ -26,30 +26,30 @@ export function* findPackagePaths(): Generator<string> {
         return gitRootPath === undefined || isInDirectory(gitRootPath, path)
     }
 
-    let cwd = process.cwd()
-    let packagePath
+    let cwd: string = process.cwd()
+    let packagePath: string | undefined
 
     while (
-        (packagePath = findUp('package.json', { type: 'file', cwd })) &&
+        (packagePath = await findUp('package.json', { type: 'file', cwd })) &&
         isInGitDirectory(packagePath)
     ) {
         yield packagePath
-        cwd = dirname(packagePath)
+        cwd = dirname(dirname(packagePath))
     }
 }
 
-export function findDependencies(
+export async function findDependencies(
     { packagePaths, keys, warnings }: {
-        packagePaths: Iterable<string>,
+        packagePaths: AsyncIterable<string> | Iterable<string>,
         keys: string[],
         warnings: string[]
     }
-): string[] {
+): Promise<string[]> {
     const dependencies: Set<string> = new Set()
 
-    for (const packagePath of packagePaths) {
+    for await (const packagePath of packagePaths) {
         try {
-            const pkg: { [key in PropertyKey]: any } = JSON.parse((readFileSync(packagePath)).toString()) ?? {}
+            const pkg: { [key in PropertyKey]: any } = JSON.parse((await fs.readFile(packagePath)).toString()) ?? {}
 
             for (const key of keys) {
                 const dependenciesToVersions: { [key in PropertyKey]: any } = pkg[key] ?? {}
