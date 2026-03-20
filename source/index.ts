819c1873e64dded7508f645a3acfffa0e59fb970
Stephan Schreiber <septh@users.noreply.github.com>
2026-03-20 14:57:19 +0100

    Refactor : move all initialization code out of buildStart hook

--- source/index.ts
+++ source/index.ts
@@ -1,8 +1,10 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import cp from 'node:child_process'
-import { createRequire, isBuiltin } from 'node:module'
+import { isBuiltin } from 'node:module'
 import type { Plugin } from 'rollup'
+
+import self from '#package.json' with { type: 'json' }
 
 type MaybeFalsy<T> = (T) | undefined | null | false
 type MaybeArray<T> = (T) | (T)[]
@@ -93,11 +95,8 @@ interface PackageJson {
     optionalDependencies?: Record<string, string>
 }
 
-// Get our own name and version
-const { name, version } = createRequire(import.meta.url)('#package.json') as PackageJson
-
 // Files that mark the root of a monorepo
-const workspaceRootFiles = [
+const monorepoRootFiles = [
     'pnpm-workspace.yaml',  // pnpm
     'lerna.json',           // Lerna / Lerna Light
     'rush.json',            // Rush
@@ -106,135 +105,146 @@ const workspaceRootFiles = [
     // 'nx.json',              // Nx
 ]
 
-// Our defaults.
-type Config = Required<ExternalsOptions>
-const defaults: Config = {
-    builtins: true,
-    builtinsPrefix: 'add',
-    packagePath: [],
-    deps: true,
-    devDeps: false,
-    peerDeps: true,
-    optDeps: true,
-    include: [],
-    exclude: []
-}
-
-// Helpers.
-const isString = (str: unknown): str is string => typeof str === 'string' && str.length > 0
-const fileExists = (name: string) => fs.stat(name).then(stat => stat.isFile()).catch(() => false)
-
 /**
  * A Rollup/Vite plugin that automatically declares NodeJS built-in modules,
  * and optionally npm dependencies, as 'external'.
  */
-function nodeExternals(options: ExternalsOptions = {}): Plugin {
+async function nodeExternals(options: ExternalsOptions = {}): Promise<Plugin> {
 
-    const config: Config = { ...defaults, ...options }
+    // Resolve options.
+    const config: Required<ExternalsOptions> = {
+        builtins: true,
+        builtinsPrefix: 'add',
+        packagePath: [],
+        deps: true,
+        devDeps: false,
+        peerDeps: true,
+        optDeps: true,
+        include: [],
+        exclude: []
+    }
 
-    let include: RegExp[] = [],     // Initialized to empty arrays
-        exclude: RegExp[] = []      // as a workaround to issue #30
+    const configWarnings: string[] = []
+    for (const [ option, value ] of Object.entries(options)) {
+        if (option in config) {
+            if (value !== undefined && value !== null)
+                (config as any)[option] = value
+        }
+        else configWarnings.push(`Ignoring unknown option '${JSON.stringify(option)}'`)
+    }
+
+    // Map the include and exclude options to arrays of regexes.
+    const [ include, exclude ] = ([ 'include', 'exclude' ] as const).map(option => ([] as Array<MaybeFalsy<string | RegExp>>)
+        .concat(config[option])
+        .reduce((result, entry, index) => {
+            if (entry instanceof RegExp)
+                result.push(entry)
+            else if (isString(entry))
+                result.push(new RegExp('^' + entry.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '$'))
+            else if (entry)
+                configWarnings.push(`Ignoring wrong entry type #${index} in '${option}' option: ${JSON.stringify(entry)}`)
+            return result
+        }, [] as RegExp[])
+    )
+
+    // Populate the packagePath option if not given by getting all package.json files
+    // from cwd up to the root of the git repo, the root of the monorepo,
+    // or the root of the volume, whichever comes first.
+    const packagePaths = ([] as string[])
+        .concat(config.packagePath)
+        .filter(isString)
+        .map(packagePath => path.resolve(packagePath))
+    if (packagePaths.length === 0) {
+
+        // Determine the root of the git repository, if any.
+        // - If git is not available, resolves with `null`. In this case, we'll fallback to the old method
+        //   of checking for a `.git` directory on each step of the 'find-up' process.
+        // - If not inside a repository, resolves with an empty string ('').
+        // - Otherwise, resolves with the path to the root of the repository.
+        const gitTopLevel = await new Promise<string | null>(resolve => {
+            cp.execFile('git', [ 'rev-parse', '--show-toplevel' ], (error, stdout) => {
+                if (error) {
+                    // - If `execFile()` failed to execute git, `error` is a `NodeJS.ErrnoException`
+                    //   and `error.code` is a string, eg. 'ENOENT' or 'EPERM'.
+                    // - If git ran but exited with non-zero, we simply assume we are not inside a git repository.
+                    resolve(typeof error.code === 'string' ? null : '')
+                }
+                else resolve(path.normalize(stdout.trim()))
+            })
+        })
+
+        for (let cwd = process.cwd(), previous = ''; previous !== cwd; previous = cwd, cwd = path.dirname(cwd)) {
+            const name = path.join(cwd, 'package.json')
+            if (await fileExists(name))
+                packagePaths.push(name)
+
+            // Break early if we are at the root of the git repo.
+            if (cwd === gitTopLevel || (gitTopLevel === null && await directoryExists(path.join(cwd, '.git'))))
+                break
+
+            // Break early if there is a known monorepo root file.
+            const breaks = await Promise.all(monorepoRootFiles.map(name => fileExists(path.join(cwd, name))))
+            if (breaks.some(Boolean))
+                break
+        }
+    }
+
+    // Gather dependencies names.
+    if (packagePaths.length > 0) {
+        const externalDependencies: Record<string, string> = {}
+
+        for (const packagePath of packagePaths) {
+            const packageJson = await fs.readFile(packagePath)
+                .then(buffer => JSON.parse(buffer.toString()) as PackageJson)
+                .catch((err: NodeJS.ErrnoException | SyntaxError) => err)
+            if (packageJson instanceof Error) {
+                const message = packageJson instanceof SyntaxError
+                    ? `File ${JSON.stringify(packagePath)} does not look like a valid package.json.`
+                    : `Cannot read ${JSON.stringify(packagePath)}, error: ${packageJson.code}.`
+                throw new Error(message, { cause: packageJson })
+            }
+
+            Object.assign(externalDependencies,
+                config.deps     ? packageJson.dependencies         : undefined,
+                config.devDeps  ? packageJson.devDependencies      : undefined,
+                config.peerDeps ? packageJson.peerDependencies     : undefined,
+                config.optDeps  ? packageJson.optionalDependencies : undefined
+            )
+
+            // Break early if this is an npm/yarn workspace root.
+            if (Array.isArray(packageJson.workspaces))
+                break
+        }
+
+        // Add all dependencies as an include RegEx.
+        const names = Object.keys(externalDependencies)
+        if (names.length > 0)
+            include.push(new RegExp('^(?:' + names.join('|') + ')(?:/.+)?$'))
+    }
 
     const isIncluded = (id: string) => include.length > 0 && include.some(rx => rx.test(id)),
           isExcluded = (id: string) => exclude.length > 0 && exclude.some(rx => rx.test(id))
 
-    // Determine the root of the git repository, if any.
-    //
-    // Note: we can't await the promise here because this would require our factory function
-    //       to be async and that would break the old Vite compatibility trick
-    //       (see issue #37 and https://github.com/vitejs/vite/issues/20717).
-    const gitTopLevel = new Promise<string>(resolve => {
-        cp.execFile('git', [ 'rev-parse', '--show-toplevel' ], (error, stdout) => {
-            resolve(error ? '' : path.normalize(stdout.trim()))
-        })
-    })
-
     return {
-        name: name.replace(/^rollup-plugin-/, ''),
-        version,
+        name: self.name.replace(/^rollup-plugin-/, ''),
+        version: self.version,
         apply: 'build',
         enforce: 'pre',
 
-        async buildStart() {
+        buildStart() {
 
-            // Map the include and exclude options to arrays of regexes.
-            [ include, exclude ] = ([ 'include', 'exclude' ] as const).map(option => ([] as Array<MaybeFalsy<string | RegExp>>)
-                .concat(config[option])
-                .reduce((result, entry, index) => {
-                    if (entry instanceof RegExp)
-                        result.push(entry)
-                    else if (isString(entry))
-                        result.push(new RegExp('^' + entry.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '$'))
-                    else if (entry)
-                        this.warn(`Ignoring wrong entry type #${index} in '${option}' option: ${JSON.stringify(entry)}`)
-                    return result
-                }, [] as RegExp[])
-            )
+            // Display initial warnings, if any, but only once.
+            let warning: string | undefined
+            while (warning = configWarnings.shift())
+                this.warn(warning)
 
-            // Populate the packagePath option if not given by getting all package.json files
-            // from cwd up to the root of the git repo, the root of the monorepo,
-            // or the root of the volume, whichever comes first.
-            const packagePaths = ([] as string[])
-                .concat(config.packagePath)
-                .filter(isString)
-                .map(packagePath => path.resolve(packagePath))
-            if (packagePaths.length === 0) {
-                for (let current = process.cwd(), previous = ''; previous !== current; previous = current, current = path.dirname(current)) {
-
-                    // Gather package.json files.
-                    const name = path.join(current, 'package.json')
-                    if (await fileExists(name))
-                        packagePaths.push(name)
-
-                    // Break early if we are at the root of the git repo
-                    // or there is a known workspace root file.
-                    const breaks = await Promise.all([
-                        gitTopLevel.then(topLevel => topLevel === current),
-                        ...workspaceRootFiles.map(name => fileExists(path.join(current, name)))
-                    ])
-
-                    if (breaks.some(result => result))
-                        break
-                }
-            }
-
-            // Gather dependencies names.
-            const externals: Record<string, string> = {}
-            for (const packagePath of packagePaths) {
-                const manifest = await fs.readFile(packagePath)
-                    .then(buffer => JSON.parse(buffer.toString()) as PackageJson)
-                    .catch((err: NodeJS.ErrnoException | SyntaxError) => err)
-                if (manifest instanceof Error) {
-                    const message = manifest instanceof SyntaxError
-                        ? `File ${JSON.stringify(packagePath)} does not look like a valid package.json.`
-                        : `Cannot read ${JSON.stringify(packagePath)}, error: ${manifest.code}.`
-                    return this.error({ message, stack: undefined })
-                }
-
-                Object.assign(externals,
-                    config.deps     ? manifest.dependencies         : undefined,
-                    config.devDeps  ? manifest.devDependencies      : undefined,
-                    config.peerDeps ? manifest.peerDependencies     : undefined,
-                    config.optDeps  ? manifest.optionalDependencies : undefined
-                )
-
-                // Watch this package.json.
-                this.addWatchFile(packagePath)
-
-                // Break early if this is an npm/yarn workspace root.
-                if (Array.isArray(manifest.workspaces))
-                    break
-            }
-
-            // Add all dependencies as an include RegEx.
-            const names = Object.keys(externals)
-            if (names.length > 0)
-                include.push(new RegExp('^(?:' + names.join('|') + ')(?:/.+)?$'))
+            // Watch all packages.json
+            packagePaths.forEach(packagePath => this.addWatchFile(packagePath))
         },
 
-        resolveId(specifier, _, { isEntry }) {
+        resolveId(specifier, importer, { isEntry }) {
             if (
-                isEntry                                 // Ignore entry points
+                !importer || isEntry                    // Ignore entry points
                 || /^(?:\0|\.{1,2}\/)/.test(specifier)  // Ignore virtual modules and relative imports
                 || path.isAbsolute(specifier)           // Ignore already resolved ids
             ) {
@@ -261,6 +271,19 @@ function nodeExternals(options: ExternalsOptions = {}): Plugin {
                 : null      // normal handling
         }
     } as ViteCompatiblePlugin
+
+    // Helpers.
+    function isString(str: unknown): str is string {
+        return typeof str === 'string' && str.length > 0
+    }
+
+    function fileExists(name: string): Promise<boolean>{
+        return fs.stat(name).then(stat => stat.isFile(), () => false)
+    }
+
+    function directoryExists(name: string): Promise<boolean>{
+        return fs.stat(name).then(stat => stat.isDirectory(), () => false)
+    }
 }
 
 export default nodeExternals
